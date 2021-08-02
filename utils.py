660cc14f1f70fd950c3e89f08a32383535bb6d54
gclen <graham.clendenning@uoit.net>
2021-08-02 16:23:45 -0400

    Initial add

---
+++ utils.py
@@ -0,0 +1,52 @@
+# Modified version of https://github.com/snorkel-team/snorkel-tutorials/blob/master/spam/utils.py
+
+import glob
+import os
+import subprocess
+from collections import OrderedDict
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+def load_spam_dataset(load_train_labels: bool = False, split_dev_valid: bool = False):
+    if os.path.basename(os.getcwd()) == "snorkel-tutorials":
+        os.chdir("spam")
+    try:
+        subprocess.run(["bash", "download_data.sh"], check=True, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print(e.stderr.decode())
+        raise e
+    filenames = sorted(glob.glob("data/Youtube*.csv"))
+
+    dfs = []
+    for i, filename in enumerate(filenames, start=1):
+        df = pd.read_csv(filename)
+        # Lowercase column names
+        df.columns = map(str.lower, df.columns)
+        # Remove comment_id field
+        df = df.drop("comment_id", axis=1)
+        # Add field indicating source video
+        df["video"] = [i] * len(df)
+        # Rename fields
+        df = df.rename(columns={"class": "label", "content": "text"})
+        # Shuffle order
+        df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+        dfs.append(df)
+
+    df_train = pd.concat(dfs[:4])
+    df_dev = df_train.sample(100, random_state=123)
+
+    if not load_train_labels:
+        df_train["label"] = np.ones(len(df_train["label"])) * -1
+    df_valid_test = dfs[4]
+    df_valid, df_test = train_test_split(
+        df_valid_test, test_size=250, random_state=123, stratify=df_valid_test.label
+    )
+
+    if split_dev_valid:
+        return df_train, df_dev, df_valid, df_test
+    else:
+        return df_train, df_test
+
